467325ca67ed42b96662435b1153bb6f31dfa5b6
Karan Chadha <kchadha@bernard.cs.arizona.edu>
2013-09-26 10:53:12 -0700

    ARP REQUEST -- kc

--- sr_router.c
+++ sr_router.c
@@ -39,7 +39,6 @@ void sr_init(struct sr_instance* sr)
 
 
 int isBroadcast(uint8_t *destMac){
-    printf(" %d ", destMac[0]);
     if((destMac[0] == 0xFF) &&
         (destMac[1] == 0xFF) &&
         (destMac[2] == 0xFF) &&
@@ -50,6 +49,15 @@ int isBroadcast(uint8_t *destMac){
     }else{
         return 0;
     }
+}
+
+
+int isARP(uint16_t type){
+	if(type == 0x0806){
+		return 1;
+	}
+	else{
+	return 0;}
 }
 
 
@@ -80,21 +88,63 @@ void sr_handlepacket(struct sr_instance* sr,
     assert(packet);
     assert(interface);
 
-uint8_t* destMac = malloc(6*sizeof(uint8_t));
 
-memcpy(destMac, packet, 6*sizeof(uint8_t));
+	uint8_t *buffer = packet;
 
-//ipv4_hdr = (IPV4_HEADER*)&packet;
-    if(isBroadcast(destMac)){
-        
-        printf("Recieved Broadcast!\n");
-    }else{
-        printf("did NOT recieve broadcast!\n");
-    }
 
-	printf("*** -> Received packet of length %d \n",len);
+	struct sr_ethernet_hdr* eth;	//Ethernet object
+	struct sr_arphdr* arp;		//ARP object
 	
-/* end sr_ForwardPacket */
+	eth = malloc(sizeof(struct sr_ethernet_hdr));
+	
+	memcpy(eth,buffer,sizeof(struct sr_ethernet_hdr));
+	
+	if(isBroadcast(eth->ether_dhost)){
+		if(isARP(ntohs(eth->ether_type))){
+			printf("\nIS broad and ARP!\n");
+		}else{
+
+			/* Code for IP packet */
+
+
+
+			printf("Broadcast but not ARP\n");
+			return;
+	}
+
+	printf("destination ethernet address = %02x:%02x:%02x:%02x:%02x:%02x\n",eth->ether_dhost[0],eth->ether_dhost[1],eth->ether_dhost[2],eth->ether_dhost[3],eth->ether_dhost[4],eth->ether_dhost[5]);
+
+printf("Source ethernet address = %02x:%02x:%02x:%02x:%02x:%02x\n",eth->ether_shost[0],eth->ether_shost[1],eth->ether_shost[2],eth->ether_shost[3],eth->ether_shost[4],eth->ether_shost[5]);
+
+	arp = malloc(sizeof(struct sr_arphdr));
+	memcpy(arp,buffer + sizeof(struct sr_ethernet_hdr),sizeof(struct sr_arphdr));
+
+	printf("Hardware type = 0x%02x\n",ntohs(arp->ar_hrd));
+	printf("Protocol type = 0x%02x\n",ntohs(arp->ar_pro));
+	printf("Hardware Size = %x\n",arp->ar_hln);
+	printf("Protocol Size = %x\n",arp->ar_pln);
+	printf("ARP opcode = 0x%02x\n",ntohs(arp->ar_op));
+	printf("Sender MAC address = %02x:%02x:%02x:%02x:%02x:%02x\n",arp->ar_sha[0],arp->ar_sha[1],arp->ar_sha[2],arp->ar_sha[3],arp->ar_sha[4],arp->ar_sha[5]);
+
+/* Calculating IP */
+    unsigned char bytes[4];
+    bytes[0] = arp->ar_sip & 0xFF;
+    bytes[1] = (arp->ar_sip >> 8) & 0xFF;
+    bytes[2] = (arp->ar_sip >> 16) & 0xFF;
+    bytes[3] = (arp->ar_sip >> 24) & 0xFF;	
+    printf("Sender IP address = %d.%d.%d.%d\n", bytes[0], bytes[1], bytes[2], bytes[3]); 
+
+	printf("Target MAC address = %02x:%02x:%02x:%02x:%02x:%02x\n",arp->ar_tha[0],arp->ar_tha[1],arp->ar_tha[2],arp->ar_tha[3],arp->ar_tha[4],arp->ar_tha[5]);
+	
+    unsigned char bytes1[4];
+    bytes1[0] = arp->ar_tip & 0xFF;
+    bytes1[1] = (arp->ar_tip >> 8) & 0xFF;
+    bytes1[2] = (arp->ar_tip >> 16) & 0xFF;
+    bytes1[3] = (arp->ar_tip >> 24) & 0xFF;	
+    printf("Target IP address = %d.%d.%d.%d\n", bytes1[0], bytes1[1], bytes1[2], bytes1[3]);
+
+}
+
 
 
 }
