1374f3cbb371119ebff4149e9727f9cc308119ed
Ubuntu <azureuser@CS525IRC.CS525IRC.d1.internal.cloudapp.net>
2013-11-13 22:32:37 +0000

    Adding first go at a server implementation --kkd

---
+++ server/echoserver.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+from twisted.internet import reactor, protocol
+from twisted.protocols.basic import LineReceiver
+import logging
+from datetime import datetime
+
+class IRC(LineReceiver):
+    def __init__(self, users):
+        #self.factory = factory
+        self.users = users
+        self.name = None
+        self.state = "GETNAME"
+
+    def connectionMade(self):
+        logger.debug("Connection was made, asking name")
+        #self.factory.clients.add(self)
+        self.transport.write("004")
+        self.sendLine("Whats your name?")
+
+    def connectionLost(self,reason):
+        if(self.name in self.users):
+            del self.users[self.name]
+            logger.debug("Connection was lost with " + self.name)
+        else:
+            logger.debug("Connection was lost with uknown")
+        #self.factory.clients.remove(self)
+
+    def lineReceived(self, line):
+        if(self.state == "GETNAME"):
+            self.handle_GETNAME(line)
+        else:
+            self.handle_CHAT(line)
+
+    def handle_GETNAME(self, name):
+        if name in self.users:
+            self.sendLine("Name taken, please choose another.")
+            return
+        self.sendLine("Welcome, %s!" % (name,))
+        self.name = name
+        self.users[name] = self
+        self.state = "CHAT"
+        logger.debug("User " + name + " added to chat.")
+
+    def handle_CHAT(self, message):
+        return
+    #def dataReceived(self, data):
+    #    self.transport.write(data)
+
+class IRCFactory(protocol.Factory):
+    def __init__(self):
+        self.users = {}
+    
+    def buildProtocol(self, addr):
+        return IRC(self.users)
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler("./server.log")
+logger.addHandler(fh)
+
+logger.debug("Server Started at " + datetime.now().isoformat())
+
+
+
+reactor.listenTCP(5000, IRCFactory())
+reactor.run()
+
+
