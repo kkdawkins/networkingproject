2af2f17e07030d59b70d402e390ae4196b193c65
Kevin Dawkins <kkdawkins@gmail.com>
2013-11-14 17:01:39 -0700

    Server time added --kkd

--- server/ircserver.py
+++ server/ircserver.py
@@ -3,24 +3,27 @@
 from twisted.internet import reactor, protocol
 from twisted.protocols.basic import LineReceiver
 import logging
-from datetime import datetime
+#from datetime import datetime
+from time import gmtime, strftime
 
 class IRC(LineReceiver):
     def __init__(self, users):
         self.users = users
         self.name = None
         self.state = "GETNAME"
+        self.serverMessage = strftime("%H:%M:%S", gmtime()) + " [Server] " # Global server message, only have to calc once
 
     def connectionMade(self):
         logger.debug("Connection was made, asking name")
         self.sendLine("004")
+        self.sendLine("[Server] ")
         self.sendLine("Whats your name?")
 
     def connectionLost(self,reason):
         if(self.name in self.users):
             del self.users[self.name]
             logger.debug("Connection was lost with " + self.name)
-            announcement = "[Server] " + self.name + " has quit."
+            announcement = self.serverMessage + self.name + " has quit."
             self.announce(announcement)
         else:
             logger.debug("Connection was lost with uknown")
@@ -40,11 +43,11 @@ class IRC(LineReceiver):
         self.users[name] = self
         self.state = "CHAT"
         logger.debug("User " + name + " added to chat.")
-        announcement = "[Server] " + name + " has joined."
+        announcement = self.serverMessage + name + " has joined."
         self.announce(announcement)
 
     def handle_CHAT(self, message):
-        toSend = "<%s> %s" % (self.name, message)
+        toSend = "%s <%s> %s" % (strftime("%H:%M:%S", gmtime()), self.name, message)
         for name, protocol in self.users.iteritems():
             if protocol != self:
                 protocol.sendLine(toSend)
